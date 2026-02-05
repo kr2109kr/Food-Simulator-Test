b70b12f7eec984bf86b1c45895acf6c50c3eeabe
kr2109kr <kr2109.kr@gmail.com>
2026-02-05 19:05:42 +0700

    Prototype_09

---
+++ Assets/Scripts/Tools/FillingStation.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class FillingStation : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameObject;
+
+    private void SelectRedBeans()
+    {
+
+    }
+
+    private void SelectCustart()
+    {
+
+    }
+
+    private void SelectChocolate()
+    {
+
+    }
+}
