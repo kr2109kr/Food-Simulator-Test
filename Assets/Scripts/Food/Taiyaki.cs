84b6144f753923c9f8500fa97125f96296f500bf
kr2109kr <kr2109.kr@gmail.com>
2026-03-19 08:39:44 +0700

    Prototype_24

--- Assets/Scripts/Food/Taiyaki.cs
+++ Assets/Scripts/Food/Taiyaki.cs
@@ -1,20 +1,37 @@
 using FoodSystem;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Taiyaki : MonoBehaviour
 {
     private TaiyakiData _taiyakiData;
+    private MeshRenderer _meshRenderer;
+
+    [SerializeField] private Material _underCookedMaterial;
+    [SerializeField] private Material _burntMaterial;
+    [SerializeField] private Material _excellentMaterial;
 
     //Test
-    private Taiyaki taiyaki = new();
+    //private Taiyaki taiyaki = new();
 
     private void Awake()
     {
         _taiyakiData = new();
+        _meshRenderer = GetComponent<MeshRenderer>();
 
         //Test
-        taiyaki.SetFillingData(TaiyakiData.Filling.RedBeans);// param: FillingBox.GetFilling();
-        taiyaki.SetDonessData(TaiyakiData.Side.Left, TaiyakiData.Doness.Excellent);// param: TaiyakiMaker.xxx();
+        //taiyaki.SetFillingData(TaiyakiData.Filling.RedBeans);// param: FillingBox.GetFilling();
+        //taiyaki.SetDonessData(TaiyakiData.Side.Left, TaiyakiData.Doness.Excellent);// param: TaiyakiMaker.xxx();
+
+        
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.qKey.wasPressedThisFrame)
+        {
+            _meshRenderer.material = _burntMaterial;
+        }
     }
 
     public void SetFillingData(TaiyakiData.Filling filling)
@@ -24,6 +41,15 @@ public class Taiyaki : MonoBehaviour
 
     public void SetDonessData(TaiyakiData.Side side, TaiyakiData.Doness doness)
     {
+        _meshRenderer.material = _burntMaterial;
         _taiyakiData.SetDoness(side, doness);
     }
+
+    public void PauseCooking()//left right
+    {
+        
+    }
+
+    
+
 }
